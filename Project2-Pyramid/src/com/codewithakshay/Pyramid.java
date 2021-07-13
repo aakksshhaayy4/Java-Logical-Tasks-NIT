72ba70440940fda9d7cb2963e0885656926d0cc2
Akshay <akhareakshay2@gmail.com>
2021-07-13 17:10:55 +0530

    Pyramid program

---
+++ Project2-Pyramid/src/com/codewithakshay/Pyramid.java
@@ -0,0 +1,41 @@
+package com.codewithakshay;
+
+public class Pyramid {
+
+	/*
+	 
+	 * 
+	 * * 
+	 * * *
+	 * * * *
+	 * * * *
+	 * * *
+	 * *
+	 *
+	 
+	 */
+
+	public static void main(String[] args) {
+
+		int n = 4;
+
+		//outer for loop is for row
+		for (int i = 1; i <= n; i++) {
+			//inner for loop is for column
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j < i; j++) {
+				System.out.print("* ");
+			}
+			System.out.print("* ");
+			System.out.println();
+		}
+
+	}
+
+}
