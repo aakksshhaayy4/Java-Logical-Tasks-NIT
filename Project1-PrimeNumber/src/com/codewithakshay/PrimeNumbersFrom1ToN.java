78565caa2eae0dc8a7fb6207b5c91be132f02370
Akshay <akhareakshay2@gmail.com>
2021-07-14 18:21:33 +0530

    prime numbers from one to n .java file

---
+++ Project1-PrimeNumber/src/com/codewithakshay/PrimeNumbersFrom1ToN.java
@@ -0,0 +1,35 @@
+package com.codewithakshay;
+
+public class PrimeNumbersFrom1ToN {
+
+	public static void main(String[] args) {
+		
+		int maxVal=100;
+		boolean isPrime=true;
+		String primeNum="";
+		int count=0;
+		
+		for(int i=2; i<=maxVal; i++) {
+			isPrime=checkPrime(i);
+			if(isPrime) {
+				primeNum=primeNum+i+" ";
+				count=count+1;
+			}
+		}
+		System.out.println("Prime Numbers from 1 to "+maxVal+" are :- "+primeNum);
+		System.out.println("TOtal Prime numbers between 1 to "+maxVal+" is :- "+count);
+
+	}
+
+	public static boolean checkPrime(int num) {
+		int rem=0;
+		for(int i=2; i<=num/2; i++) {
+			rem=num%i;
+			if(rem==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
